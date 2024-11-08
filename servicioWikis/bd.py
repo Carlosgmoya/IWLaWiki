7574e95d4234b8d28c7164304b76186dd623a195
dani-dlg <100839577+dani-dlg@users.noreply.github.com>
2024-11-08 18:23:39 +0100

    crear estructura de microservicios con conexion a bd

---
+++ servicioWikis/bd.py
@@ -0,0 +1,20 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://admin:admin@cluster0.mw2bq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Wiki conectado a MongoDB!")
+except Exception as e:
+    print(e)
+
+# Base de Datos
+
+database = client["laWiki"]
+
+BD_wiki = database["wiki"]
