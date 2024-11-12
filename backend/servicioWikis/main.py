1011e2eebe46372f659b1514f0cde8757c9a275b
Alex <alexfusun@gmail.com>
2024-11-12 16:46:36 +0100

    Refactorizacion: Limpiado codigo en servicioWikis, eliminado librerias no usadas. Actualizado contenedor Docker

--- backend/servicioWikis/main.py
+++ backend/servicioWikis/main.py
@@ -1,46 +1,31 @@
 from services import wiki as wikiAPI
 
 from fastapi import FastAPI, Request, HTTPException, Query
-
-from typing import Any, Union
-from bson import json_util
+from typing import Any
 from bson.objectid import ObjectId
 from typing import List
-import json
-from datetime import datetime
-from fastapi.middleware.cors import CORSMiddleware
 
 
-
-app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Cambia al puerto de tu frontend en React
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+api = FastAPI()
 
 # prefijo para todas las URLs
 path = "/api/v1"
 
-# Desde el directorio de este archivo: 2 maneras de ejecutar la app:
+# Desde el directorio de este archivo: 2 maneras de ejecutar el modulo:
 #   -ejecutar de manera local -> python -m uvicorn main:api --reload --port 8001
 #   -ejecutar el contenedor Docker -> docker run -p 8001:8001 fu17alex/modulowiki:v1.0
 
 
 # PAGINA PRINCIPAL: DEVUELVE TODAS LAS WIKIS O DEVUELVE LAS WIKIS QUE CUMPLEN UNOS CRITERIOS
-@app.get(path + "/wikis")
-async def getWikis(term: str = Query(None, min_length=1)):  #SOLO ES UN ESQUEMA, FALTA POR IMPLEMENTAR
+@api.get(path + "/wikis")
+async def getWikis(term: str = Query(None, min_length=1)):
     return await wikiAPI.getTodasWikis() if term is None else await wikiAPI.getWikisPorNombre(term)
 
 
 # PAGINA WIKI
-@app.get(path + "/wikis/{nombre}")
+@api.get(path + "/wikis/{nombre}")
 async def getWiki(nombre: str):
     wikiJSON = await wikiAPI.getWiki(nombre)
-
     if wikiJSON is None:
         raise HTTPException(status_code=404, detail="Wiki no encontrado")
     
@@ -48,19 +33,18 @@ async def getWiki(nombre: str):
     
     
 # CREAR WIKI
-@app.post(path + "/wikis")
+@api.post(path + "/wikis")
 async def crearWiki(request: Request):
     data = await request.json()
     nombre = data.get("nombre")
     descripcion = data.get("descripcion")
-
     wikiJSON = await wikiAPI.getWiki(nombre)
 
     return await wikiAPI.crearWiki(nombre, descripcion) if wikiJSON is None else "Ya existe una wiki con ese nombre"
 
 
 # ACTUALIZAR WIKI
-@app.put(path + "/wikis/{wikiID}")
+@api.put(path + "/wikis/{wikiID}")
 async def actualizarWiki(request: Request, wikiID: str):
     try:
         ObjID = ObjectId(wikiID)
@@ -73,10 +57,8 @@ async def actualizarWiki(request: Request, wikiID: str):
     descripcion = data.get("descripcion")
     
     result = await wikiAPI.actualizarWiki(ObjID, nombre, descripcion)
-    
     if result.matched_count == 0:
         raise HTTPException(status_code=404, detail="Wiki no encontrada")
-    
     if result.modified_count == 0:
         return "No se realizaron cambios"
     
@@ -84,7 +66,7 @@ async def actualizarWiki(request: Request, wikiID: str):
 
 
 # ELIMINAR WIKI
-@app.delete(path + "/wikis/{wikiID}")
+@api.delete(path + "/wikis/{wikiID}")
 async def eliminarWiki(wikiID: str):
     try:
         obj_id = ObjectId(wikiID)
@@ -92,7 +74,6 @@ async def eliminarWiki(wikiID: str):
         raise HTTPException(status_code=400, detail="Formato de ID inválido")
     
     result = await wikiAPI.eliminarWiki(obj_id)
-
     if result.deleted_count == 0:
         raise HTTPException(status_code=404, detail="Wiki no encontrada")
 
