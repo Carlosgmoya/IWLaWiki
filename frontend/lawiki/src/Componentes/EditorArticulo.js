dc2a79bc7e41ecc99507aaf959c66600759fbb71
Pedro <139225059+PeterWheel77@users.noreply.github.com>
2024-12-25 01:44:07 +0100

    Commit navideño
    parseador

--- frontend/lawiki/src/Componentes/EditorArticulo.js
+++ frontend/lawiki/src/Componentes/EditorArticulo.js
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import SubirMapa from "./SubirMapa";
 import SubirImagen from "./SubirImagen";
 
 const EditorArticulo = ({ nombreWiki, tituloArticulo, contenidoInicial, onCancelar }) => {
   const [contenido, setContenido] = useState(contenidoInicial);
   const [mensaje, setMensaje] = useState("");
+  const [mostrarFormulario, setMostrarFormulario] = useState(true); // Estado para alternar formulario/mensaje 
+
+  const referenciaTextArea = useRef(null);
+
 
   // Cambia esto por el ID real del creador que corresponda a tu contexto.
   const creadorId = "671fcdfdf45c1f9bfed57032";
@@ -28,10 +32,11 @@ const EditorArticulo = ({ nombreWiki, tituloArticulo, contenidoInicial, onCancel
           body: JSON.stringify(datos),
         }
       );
+      
 
       if (response.ok) {
         setMensaje("¡Guardado exitosamente!");
-        onCancelar();
+        
       } else {
         const errorData = await response.json();
         console.error("Error:", errorData);
@@ -41,30 +46,113 @@ const EditorArticulo = ({ nombreWiki, tituloArticulo, contenidoInicial, onCancel
       console.error("Error:", error);
       setMensaje("Error inesperado al conectar con el servidor.");
     }
+    setMostrarFormulario(false);
   };
 
+  const handleTitulo = async () => {
+    insertarTexto("# Inserta aquí tu título");
+  };
+
+  const handleSubtitulo = async () => {
+    insertarTexto("## Inserta aquí tu Subtítulo");
+  };
+
+  const handleCursiva = async () => {
+    insertarTexto("*cursiva*");
+  };
+
+  const handleNegrita = async () => {
+    insertarTexto("**negrita**");
+  };
+
+  const handleCursivayNegrita = async () => {
+    insertarTexto("***cursiva y negrita***");
+  };
+
+  const handleEnlaceOtraPagina = async () => {
+    insertarTexto("[Enlace a otra página](https://otra-pagina.com)");
+  };
+
+  const handleEnlaceImagen = async () => {
+    insertarTexto("![Descripción de la imagen](url de la imagen)");
+  };
+
+  const handleLista = async () => {
+    insertarTexto("- elemento de la lista");
+  };
+
+
+
+  const insertarTexto = (textoAInsertar) => {
+    const textArea = referenciaTextArea.current;
+
+    if (textArea) {
+      const inicio = textArea.selectionStart;
+      const final = textArea.selectionEnd;
+
+      // Inserto el texto donde estaba el cursos
+      const nuevoContenido =
+        contenido.slice(0, inicio) +
+        textoAInsertar +
+        contenido.slice(final);
+
+      setContenido(nuevoContenido);
+
+      // Actualizo la posicion del cursor
+      setTimeout(() => {
+        textArea.setSelectionRange(
+          inicio + textoAInsertar.length,
+          inicio + textoAInsertar.length
+        );
+      }, 0);
+    }
+  };
+
+
+
   return (
-    <div style={{ padding: "20px" }}>
-      <h2>Editar Artículo</h2>
-      <textarea
-        value={contenido}
-        onChange={handleChange}
-        rows="10"
-        cols="50"
-      />
-      <button onClick={handleGuardar}>Guardar</button>
-      {mensaje && <div style={{ marginTop: "10px" }}>{mensaje}</div>}
-      <div>
-        <h1>Subir Imagen</h1>
-        <SubirImagen />
-      </div>
-      <div>
-        <SubirMapa
-          nombreWiki={nombreWiki}
-          tituloArticulo={tituloArticulo}
-        />
-      </div>
-    </div>
+    <>
+      {mostrarFormulario ? (
+        <div style={{ padding: "20px" }}>
+          <h2>Editar Artículo</h2>
+          <div className="BotonesEditor">
+            <button onClick={handleTitulo}>Insertar Titulo</button>
+            <button onClick={handleSubtitulo}>Insertar Subtitulo</button>
+            <button onClick={handleCursiva}>Insertar Cursiva</button>
+            <button onClick={handleNegrita}>Insertar Negrita</button>
+            <button onClick={handleCursivayNegrita}>Insertar cursiva y negrita</button>
+            <button onClick={handleEnlaceOtraPagina}>Insertar Enlace</button>
+            <button onClick={handleEnlaceImagen}>Insertar Enlace Imagen</button>
+            <button onClick={handleLista}>Insertar Lista</button>
+          </div>
+
+
+          <textarea
+            ref={referenciaTextArea} // Asocia la referencia al textarea
+            value={contenido}
+            onChange={handleChange}
+            rows="10"
+            cols="50"
+          />
+          <button onClick={handleGuardar}>Guardar</button>
+          
+          <div>
+            <h1>Subir Imagen</h1>
+            <SubirImagen />
+          </div>
+          <div>
+            <SubirMapa
+              nombreWiki={nombreWiki}
+              tituloArticulo={tituloArticulo}
+            />
+          </div>
+        </div>
+      ) : (
+        <>
+          {mensaje && <div style={{ marginTop: "10px" }}>{mensaje}</div>}
+        </>
+      )}
+    </>
   );
 };
 
